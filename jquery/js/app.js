b2343b10e637f5e091cbcc02f728e4f75c5036ee
Eric Hoanshelt <erichoanshelt@Eric-Hoanshelts-MacBook-Pro.local>
2015-08-10 19:56:08 -0500

    jQuery fun

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $("blockquote").text("Hello World!");
+    
+    setTimeout(function(){
+       $("blockquote").text("Are you still looking at me?");
+    }, 2000);
 });
